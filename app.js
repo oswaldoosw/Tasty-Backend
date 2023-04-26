3ef908e0c43a0fe2a61ecc944cb9e52ca4edc2b1
OSWALDO <oswaldoromy@gmail.com>
2023-04-27 02:10:01 +0700

    trying to deploy :( (2)

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config()
 
 const app = express();
 const user = require('./routes/user');
+
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -22,7 +26,7 @@ mongoose.connect(mongoUrl, {
 
 app.use('/', user);
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log("Server Started");
 });
