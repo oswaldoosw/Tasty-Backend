561839ac12591966736a985cc908d47b531450fb
OSWALDO <oswaldoromy@gmail.com>
2023-04-27 01:47:02 +0700

    configured port

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ mongoose.connect(mongoUrl, {
 
 app.use('/', user);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log("Server Started");
 });
 
