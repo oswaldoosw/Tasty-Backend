ce5fbbf6dbda38fe62d88bfe7545fe01ed0c50c1
OSWALDO <oswaldoromy@gmail.com>
2023-04-27 01:24:20 +0700

    removed unused module

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const user = require('./routes/user');
-app.use(express.static(path.join(__dirname, "/public")));
 app.use(cors());
 app.use(express.json());
 
